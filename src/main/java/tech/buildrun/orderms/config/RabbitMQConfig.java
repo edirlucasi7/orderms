b55dbd4a85b5b02a1dad29a16c49271b710ae75d
Edir Lucas Icety <icetylucas@gmail.com>
2024-08-17 11:45:54 -0300

    update nome fila e banco

--- src/main/java/tech/buildrun/orderms/config/RabbitMQConfig.java
+++ src/main/java/tech/buildrun/orderms/config/RabbitMQConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-    public static final String ORDER_CREATED_QUEUE = "btg-pactual-order-created";
+    public static final String ORDER_CREATED_QUEUE = "order-created";
 
     @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
